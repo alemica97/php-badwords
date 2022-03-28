47887b21b6e14569edb06ee23b6dbe48ce8795f1
alemica97 <alessandro.micalizzi08@gmail.com>
2022-03-28 16:23:47 +0200

    con strlen ho stampato il valore della lunghezza del paragrafo

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
     $paragrafo = "PHP (acronimo ricorsivo di 'PHP: Hypertext Preprocessor', preprocessore di ipertesti; originariamente acronimo di 'Personal Home Page')
                  è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche.";
-
+    // Stampo la variabile che contiene il paragrafo 
     echo $paragrafo;
+    // Stampo la lunghezza della variabile, ovvero del paragrafo 
+    echo strlen($paragrafo);
 ?>
