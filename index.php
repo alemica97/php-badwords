b79ff85b2bdd939970d3579723f16093e36901ab
alemica97 <alessandro.micalizzi08@gmail.com>
2022-03-28 17:03:53 +0200

    tramite $_GET  l'utente sceglie la parola da censurare

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     $paragrafo = "PHP (acronimo ricorsivo di 'PHP: Hypertext Preprocessor', preprocessore di ipertesti; originariamente acronimo di 'Personal Home Page')
                  è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche.";
 
-    $paragrafo_modificato = str_replace('PHP', '***', $paragrafo);
+
+    $parola_da_censurare = $_GET['censura'];
+    $paragrafo_modificato = str_replace( $parola_da_censurare, '***', $paragrafo);
 
     // Stampo la variabile che contiene il paragrafo 
     echo $paragrafo;
@@ -13,4 +15,8 @@
     // Stampo il paragrafo modificato, che avrà la parola PHP censurata
     echo '<br>';
     echo $paragrafo_modificato;
+
+    echo '<br>';
+    echo 'Il paragrafo censurato è lungo: ';
+    echo strlen($paragrafo_modificato);
 ?>
